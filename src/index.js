7fecb43ca4984ea5a032532a2a8624a9b6014f9b
Tom Davidson <tom@davidson.me.uk>
2019-12-20 16:39:21 +0000

    contentful paint metric and error handling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
+const url = require('url');
 const express = require('express');
 const Prometheus = require('prom-client');
 const promMid = require('express-prometheus-middleware');
 const pagespeed = require('./pagespeed');
 const apiKey = process.env.APIKey;
+const pages = process.env.PAGES.split(',').map((page) => page.trim());
 const PORT = 3000;
+const pollInterval = 1; //minutes
 
-const gauge = new Prometheus.Gauge({ name: 'test_metric_name', help: 'test_metric_help' });
-gauge.set(10); // Set to 10
-gauge.inc(); // Inc with 1
+const fs = require('fs');
+
+for (let page of pages) {
+	urlparts = url.parse(page);
+	let metric_name = `first_contentful_paint__${urlparts.hostname.replace(
+		/[^a-zA-Z0-9]/g,
+		'_'
+	)}${urlparts.pathname.replace(/\/$/, '').replace(/[^a-zA-Z0-9]/g, '_')}`;
+	let metric_help = `Time to first contentful paint for ${page}`;
+	let gauge = new Prometheus.Gauge({ name: metric_name, help: metric_help });
+	setInterval(async () => {
+		let data = await pagespeed(page, apiKey);
+		if (data != null) {
+			try {
+				let ttfp = data.lighthouseResult.audits['first-contentful-paint'].numericValue;
+				gauge.set(ttfp);
+			} catch (e) {
+				console.log(`data parsing failed, response dumped, url called: ${page}`);
+				fs.writeFileSync('response.json', data, { mode: 0o755 });
+			}
+		}
+	}, pollInterval * 60 * 1000);
+}
 
 const app = express();
 app.use(
@@ -24,9 +47,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
 	console.log(`Example api is listening on http://localhost:${PORT}`);
 });
-
-// (async () => {
-//     let page = 'https://developers.google.com';
-// 	let data = await pagespeed(page, apiKey);
-// 	console.log(data);
-// })();
--- src/pagespeed.js
+++ src/pagespeed.js
@@ -2,12 +2,12 @@ const axios = require('axios');
 
 module.exports = async function(page, apiKey) {
 	let url = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${page}&key=${apiKey}`;
-	console.log(`fetching ${url}`);
 	try {
 		const response = await axios.get(url);
 		const data = response.data;
 		return data;
 	} catch (error) {
-		return error;
+		console.log(`Error fetching ${page} - ${error}`)
+		return null;
 	}
 };
