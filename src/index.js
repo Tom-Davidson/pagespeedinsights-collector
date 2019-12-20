5404814455f513a17c33ced29722b89c5e457719
Tom Davidson <tom@davidson.me.uk>
2019-12-20 14:43:11 +0000

    /metric endpoint and custom metric

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
+const express = require('express');
+const Prometheus = require('prom-client');
+const promMid = require('express-prometheus-middleware');
 const pagespeed = require('./pagespeed');
 const apiKey = process.env.APIKey;
+const PORT = 3000;
 
-(async () => {
-    let page = 'https://developers.google.com';
-	let data = await pagespeed(page, apiKey);
-	console.log(data);
-})();
+const gauge = new Prometheus.Gauge({ name: 'test_metric_name', help: 'test_metric_help' });
+gauge.set(10); // Set to 10
+gauge.inc(); // Inc with 1
+
+const app = express();
+app.use(
+	promMid({
+		metricsPath: '/metrics',
+		collectDefaultMetrics: true,
+		requestDurationBuckets: [ 0.1, 0.5, 1, 1.5 ]
+	})
+);
+app.get('/', (req, res) => {
+	res.set('location', './metrics');
+	res.status(301).send();
+});
+app.listen(PORT, () => {
+	console.log(`Example api is listening on http://localhost:${PORT}`);
+});
+
+// (async () => {
+//     let page = 'https://developers.google.com';
+// 	let data = await pagespeed(page, apiKey);
+// 	console.log(data);
+// })();
