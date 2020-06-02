5a5942b415e2673edefe28a38504bb8634a171c9
Tom Davidson <tom@davidson.me.uk>
2020-06-02 14:42:48 +0100

    moved from a metric per page to a metric with labels

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-const url = require('url');
+const fs = require('fs');
 const express = require('express');
 const Prometheus = require('prom-client');
 const promMid = require('express-prometheus-middleware');
 const pagespeed = require('./pagespeed');
+
 const apiKey = process.env.APIKey;
 const pages = process.env.PAGES.split(',').map((page) => page.trim());
 const PORT = 3000;
 const pollInterval = 1; //minutes
 
-const fs = require('fs');
+const metrics = {
+	first_contentful_paint: new Prometheus.Gauge({
+		name: 'first_contentful_paint',
+		help: 'Time to first contentful paint, more info here: https://web.dev/first-contentful-paint/',
+		labelNames: [ 'page' ]
+	})
+};
 
 for (let page of pages) {
-	urlparts = url.parse(page);
-	let metric_name = `first_contentful_paint__${urlparts.hostname.replace(
-		/[^a-zA-Z0-9]/g,
-		'_'
-	)}${urlparts.pathname.replace(/\/$/, '').replace(/[^a-zA-Z0-9]/g, '_')}`;
-	let metric_help = `Time to first contentful paint for ${page}`;
-	let gauge = new Prometheus.Gauge({ name: metric_name, help: metric_help });
 	setInterval(async () => {
 		let data = await pagespeed(page, apiKey);
 		if (data != null) {
 			try {
 				let ttfp = data.lighthouseResult.audits['first-contentful-paint'].numericValue;
-				gauge.set(ttfp);
+				metrics.first_contentful_paint.set({ page }, ttfp);
 			} catch (e) {
-				console.log(`data parsing failed, response dumped, url called: ${page}`);
+				console.error(`data parsing failed, response dumped, url called: ${page}`);
 				fs.writeFileSync('response.json', data, { mode: 0o755 });
 			}
 		}
@@ -44,7 +44,8 @@ app.get('/', (req, res) => {
 	res.set('location', './metrics');
 	res.status(301).send();
 });
-app.get('/healthz', (req, res) => { // Kubernetes liveness probe endpoint
+app.get('/healthz', (req, res) => {
+	// Kubernetes liveness probe endpoint
 	res.status(200).send();
 });
 app.listen(PORT, () => {
