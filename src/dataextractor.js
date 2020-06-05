2339bdd772f2c9e8cf2ff84d3737448204981e5c
Tom Davidson <tom@davidson.me.uk>
2020-06-05 13:13:41 +0100

    dataextractor: return undefined when the data is not in the structure we expect

--- src/dataextractor.js
+++ src/dataextractor.js
@@ -1,14 +1,24 @@
 const collect_numeric_from_audits = (data, key) => {
-    return data.lighthouseResult.audits[key].numericValue;
-}
+	if (
+        data &&
+		data.lighthouseResult &&
+		data.lighthouseResult.audits &&
+		data.lighthouseResult.audits[key] &&
+		data.lighthouseResult.audits[key].numericValue
+	) {
+		return data.lighthouseResult.audits[key].numericValue;
+	} else {
+		return undefined;
+	}
+};
 const first_contentful_paint = (data) => {
-    return collect_numeric_from_audits(data, 'first-contentful-paint')
+	return collect_numeric_from_audits(data, 'first-contentful-paint');
 };
 const first_cpu_idle = (data) => {
-    return collect_numeric_from_audits(data, 'first-cpu-idle')
+	return collect_numeric_from_audits(data, 'first-cpu-idle');
 };
 
 module.exports = {
-    first_contentful_paint,
-    first_cpu_idle
+	first_contentful_paint,
+	first_cpu_idle
 };
--- tests/dataextractor.test.js
+++ tests/dataextractor.test.js
@@ -5,6 +5,10 @@ test('dataextractor.first_contentful_paint', () => {
 	const fcp = dataextractor.first_contentful_paint(data);
 	expect(fcp).toBe(660);
 });
+test('dataextractor.first_contentful_paint with bad data', () => {
+	const fcp = dataextractor.first_contentful_paint(undefined);
+	expect(fcp).toBe(undefined);
+});
 test('dataextractor.first_cpu_idle', () => {
 	const fci = dataextractor.first_cpu_idle(data);
 	expect(fci).toBe(2979);
