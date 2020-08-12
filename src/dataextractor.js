0d679e0dc1c0fd8132ecd31c341c6c4bf3b1adcb
Philip Marsey <philip.marsey@sky.uk>
2020-08-12 15:00:44 +0100

    Added additional metric and renamed to follow recommended naming convensions

--- src/dataextractor.js
+++ src/dataextractor.js
@@ -11,14 +11,43 @@ const collect_numeric_from_audits = (data, key) => {
 		return undefined;
 	}
 };
+
 const first_contentful_paint = (data) => {
 	return collect_numeric_from_audits(data, 'first-contentful-paint');
 };
 const first_cpu_idle = (data) => {
 	return collect_numeric_from_audits(data, 'first-cpu-idle');
 };
+const interactive = (data) => {
+	return collect_numeric_from_audits(data, 'interactive');
+};
+const speed_index = (data) => {
+	return collect_numeric_from_audits(data, 'speed-index');
+};
+const max_potential_fid = (data) => {
+	return collect_numeric_from_audits(data, 'max-potential-fid');
+};
+const first_meaningful_paint = (data) => {
+	return collect_numeric_from_audits(data, 'first-meaningful-paint');
+};
+const performance_score = (data) => {
+	if (data && 
+		data.lighthouseResult && 
+		data.lighthouseResult.categories && 
+		data.lighthouseResult.categories.performance &&
+		data.lighthouseResult.categories.performance.score
+	) {
+		return data.lighthouseResult.categories.performance.score;
+	}
+	return undefined;
+};
 
 module.exports = {
 	first_contentful_paint,
-	first_cpu_idle
+	first_cpu_idle,
+	interactive,
+	speed_index,
+	max_potential_fid,
+	first_meaningful_paint,
+	performance_score
 };
--- src/index.js
+++ src/index.js
@@ -12,14 +12,38 @@ const pollInterval = 1; //minutes
 
 const metrics = {
 	first_contentful_paint: new Prometheus.Gauge({
-		name: 'first_contentful_paint',
+		name: 'pagespeed_first_contentful_paint_milliseconds',
 		help: 'Time to first contentful paint, more info here: https://web.dev/first-contentful-paint/',
 		labelNames: [ 'page' ]
 	}),
 	first_cpu_idle: new Prometheus.Gauge({
-		name: 'first_cpu_idle',
-		help:
-			"First CPU Idle marks the first time at which the page's main thread is quiet enough to handle input.  https://web.dev/first-cpu-idle.",
+		name: 'pagespeed_first_cpu_idle_milliseconds',
+		help: "First CPU Idle marks the first time at which the page's main thread is quiet enough to handle input.  https://web.dev/first-cpu-idle.",
+		labelNames: [ 'page' ]
+	}),
+	interactive: new Prometheus.Gauge({
+		name: 'pagespeed_interactive_milliseconds',
+		help: "Time to interactive is the amount of time it takes for the page to become fully interactive. https://web.dev/interactive.",
+		labelNames: [ 'page' ]
+	}),
+	speed_index: new Prometheus.Gauge({
+		name: 'pagespeed_speed_index_seconds',
+		help: "Speed Index, more info here: https://web.dev/speed-index",
+		labelNames: [ 'page' ]
+	}),
+	max_potential_fid: new Prometheus.Gauge({
+		name: 'pagespeed_max_potential_fid_seconds',
+		help: "The maximum potential First Input Delay that your users could experience is the duration, in milliseconds, of the longest task. https://developers.google.com/web/updates/2018/05/first-input-delay",
+		labelNames: [ 'page' ]
+	}),
+	first_meaningful_paint: new Prometheus.Gauge({
+		name: 'pagespeed_first_meaningful_paint_seconds',
+		help: "First Meaningful Paint measures when the primary content of a page is visible. https://web.dev/first-meaningful-paint",
+		labelNames: [ 'page' ]
+	}),
+	performance_score: new Prometheus.Gauge({
+		name: 'pagespeed_performance_score',
+		help: "Performance Score",
 		labelNames: [ 'page' ]
 	})
 };
@@ -29,9 +53,13 @@ for (let page of pages) {
 		let data = await pagespeed(page, apiKey);
 		if (data != null) {
 			try {
-				let ttfp = dataextractor.first_contentful_paint(data);
-				metrics.first_contentful_paint.set({ page }, ttfp);
+				metrics.first_contentful_paint.set({ page }, dataextractor.first_contentful_paint(data));
 				metrics.first_cpu_idle.set({ page }, dataextractor.first_cpu_idle(data));
+				metrics.interactive.set({ page }, dataextractor.interactive(data));
+				metrics.speed_index.set({ page }, dataextractor.speed_index(data));
+				metrics.max_potential_fid.set({ page }, dataextractor.max_potential_fid(data));
+				metrics.first_meaningful_paint.set({ page }, dataextractor.first_meaningful_paint(data));
+				metrics.performance_score.set({ page }, dataextractor.performance_score(data));
 			} catch (e) {
 				console.error(`data parsing failed, response dumped, url called: ${page}`);
 				fs.writeFileSync('response.json', data, { mode: 0o755 });
