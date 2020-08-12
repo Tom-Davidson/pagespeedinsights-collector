8f2d723a328282b41997af76fc71d56aeb71a9b2
Philip Marsey <philip.marsey@sky.uk>
2020-08-12 15:39:55 +0100

    tests for new metrics

--- tests/dataextractor.test.js
+++ tests/dataextractor.test.js
@@ -13,3 +13,23 @@ test('dataextractor.first_cpu_idle', () => {
 	const fci = dataextractor.first_cpu_idle(data);
 	expect(fci).toBe(2979);
 });
+test('dataextractor.interactive', () => {
+	const tti = dataextractor.interactive(data);
+	expect(tti).toBe(3137.5);
+});
+test('dataextractor.speed_index', () => {
+	const si = dataextractor.speed_index(data);
+	expect(si).toBe(1155.7224822875853);
+});
+test('dataextractor.max_potential_fid', () => {
+	const mpf = dataextractor.max_potential_fid(data);
+	expect(mpf).toBe(172);
+});
+test('dataextractor.first_meaningful_paint', () => {
+	const fmp = dataextractor.first_meaningful_paint(data);
+	expect(fmp).toBe(973);
+});
+test('dataextractor.performance_score', () => {
+	const ps = dataextractor.performance_score(data);
+	expect(ps).toBe(0.87);
+});
