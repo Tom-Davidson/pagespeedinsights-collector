902afd589deb483d956be738cc582bf60452df75
Stuart Thompson <sbthompson@users.noreply.github.com>
2020-11-04 16:26:06 +0000

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ const metrics = {
 		labelNames: [ 'page' ]
 	}),
 	accessibility_score: new Prometheus.Gauge({
-		name: 'pagespeed_accessibility_performance_score',
+		name: 'pagespeed_accessibility_score',
 		help: "Accessibility Score",
 		labelNames: [ 'page' ]
 	})
