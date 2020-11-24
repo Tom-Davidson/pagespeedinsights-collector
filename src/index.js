592b373e0e30fd4647b53cec2b5321d2dfe725c6
joreavell <joanne.reavell@sky.uk>
2020-11-24 12:23:29 +0000

    add guages for mobile,update query to use strategy

--- src/index.js
+++ src/index.js
@@ -15,43 +15,83 @@ if (isNaN(pollInterval)) {
 
 
 const metrics = {
-	first_contentful_paint: new Prometheus.Gauge({
-		name: 'pagespeed_first_contentful_paint_milliseconds',
+	desktop_first_contentful_paint: new Prometheus.Gauge({
+		name: 'desktop_pagespeed_first_contentful_paint_milliseconds',
 		help: 'Time to first contentful paint, more info here: https://web.dev/first-contentful-paint/',
 		labelNames: [ 'page' ]
 	}),
-	first_cpu_idle: new Prometheus.Gauge({
-		name: 'pagespeed_first_cpu_idle_milliseconds',
+	desktop_first_cpu_idle: new Prometheus.Gauge({
+		name: 'desktop_pagespeed_first_cpu_idle_milliseconds',
 		help: "First CPU Idle marks the first time at which the page's main thread is quiet enough to handle input.  https://web.dev/first-cpu-idle.",
 		labelNames: [ 'page' ]
 	}),
-	interactive: new Prometheus.Gauge({
-		name: 'pagespeed_interactive_milliseconds',
+	desktop_interactive: new Prometheus.Gauge({
+		name: 'desktop_pagespeed_interactive_milliseconds',
 		help: "Time to interactive is the amount of time it takes for the page to become fully interactive. https://web.dev/interactive.",
 		labelNames: [ 'page' ]
 	}),
-	speed_index: new Prometheus.Gauge({
-		name: 'pagespeed_speed_index_seconds',
+	desktop_speed_index: new Prometheus.Gauge({
+		name: 'desktop_pagespeed_speed_index_seconds',
 		help: "Speed Index, more info here: https://web.dev/speed-index",
 		labelNames: [ 'page' ]
 	}),
-	max_potential_fid: new Prometheus.Gauge({
-		name: 'pagespeed_max_potential_fid_seconds',
+	desktop_max_potential_fid: new Prometheus.Gauge({
+		name: 'desktop_pagespeed_max_potential_fid_seconds',
 		help: "The maximum potential First Input Delay that your users could experience is the duration, in milliseconds, of the longest task. https://developers.google.com/web/updates/2018/05/first-input-delay",
 		labelNames: [ 'page' ]
 	}),
-	first_meaningful_paint: new Prometheus.Gauge({
-		name: 'pagespeed_first_meaningful_paint_seconds',
+	desktop_first_meaningful_paint: new Prometheus.Gauge({
+		name: 'desktop_pagespeed_first_meaningful_paint_seconds',
 		help: "First Meaningful Paint measures when the primary content of a page is visible. https://web.dev/first-meaningful-paint",
 		labelNames: [ 'page' ]
 	}),
-	performance_score: new Prometheus.Gauge({
-		name: 'pagespeed_performance_score',
+	desktop_performance_score: new Prometheus.Gauge({
+		name: 'desktop_pagespeed_performance_score',
 		help: "Performance Score",
 		labelNames: [ 'page' ]
 	}),
-	accessibility_score: new Prometheus.Gauge({
-		name: 'pagespeed_accessibility_score',
+	desktop_accessibility_score: new Prometheus.Gauge({
+		name: 'desktop_pagespeed_accessibility_score',
+		help: "Accessibility Score",
+		labelNames: [ 'page' ]
+	}),
+	mobile_first_contentful_paint: new Prometheus.Gauge({
+		name: 'mobile_pagespeed_first_contentful_paint_milliseconds',
+		help: 'Time to first contentful paint, more info here: https://web.dev/first-contentful-paint/',
+		labelNames: [ 'page' ]
+	}),
+	mobile_first_cpu_idle: new Prometheus.Gauge({
+		name: 'mobile_pagespeed_first_cpu_idle_milliseconds',
+		help: "First CPU Idle marks the first time at which the page's main thread is quiet enough to handle input.  https://web.dev/first-cpu-idle.",
+		labelNames: [ 'page' ]
+	}),
+	mobile_interactive: new Prometheus.Gauge({
+		name: 'mobile_pagespeed_interactive_milliseconds',
+		help: "Time to interactive is the amount of time it takes for the page to become fully interactive. https://web.dev/interactive.",
+		labelNames: [ 'page' ]
+	}),
+	mobile_speed_index: new Prometheus.Gauge({
+		name: 'mobile_pagespeed_speed_index_seconds',
+		help: "Speed Index, more info here: https://web.dev/speed-index",
+		labelNames: [ 'page' ]
+	}),
+	mobile_max_potential_fid: new Prometheus.Gauge({
+		name: 'mobile_pagespeed_max_potential_fid_seconds',
+		help: "The maximum potential First Input Delay that your users could experience is the duration, in milliseconds, of the longest task. https://developers.google.com/web/updates/2018/05/first-input-delay",
+		labelNames: [ 'page' ]
+	}),
+	mobile_first_meaningful_paint: new Prometheus.Gauge({
+		name: 'mobile_pagespeed_first_meaningful_paint_seconds',
+		help: "First Meaningful Paint measures when the primary content of a page is visible. https://web.dev/first-meaningful-paint",
+		labelNames: [ 'page' ]
+	}),
+	mobile_performance_score: new Prometheus.Gauge({
+		name: 'mobile_pagespeed_performance_score',
+		help: "Performance Score",
+		labelNames: [ 'page' ]
+	}),
+	mobile_accessibility_score: new Prometheus.Gauge({
+		name: 'mobile_pagespeed_accessibility_score',
 		help: "Accessibility Score",
 		labelNames: [ 'page' ]
 	})
@@ -59,20 +99,36 @@ const metrics = {
 
 for (let page of pages) {
 	setInterval(async () => {
-		let data = await pagespeed(page, apiKey);
-		if (data != null) {
+		let desktopData = await pagespeed(page, apiKey, 'DESKTOP');
+		if (desktopData != null) {
 			try {
-				metrics.first_contentful_paint.set({ page }, dataextractor.first_contentful_paint(data));
-				metrics.first_cpu_idle.set({ page }, dataextractor.first_cpu_idle(data));
-				metrics.interactive.set({ page }, dataextractor.interactive(data));
-				metrics.speed_index.set({ page }, dataextractor.speed_index(data));
-				metrics.max_potential_fid.set({ page }, dataextractor.max_potential_fid(data));
-				metrics.first_meaningful_paint.set({ page }, dataextractor.first_meaningful_paint(data));
-				metrics.performance_score.set({ page }, dataextractor.performance_score(data));
-				metrics.accessibility_score.set({ page }, dataextractor.accessibility_score(data));
+				metrics.desktop_first_contentful_paint.set({ page }, dataextractor.first_contentful_paint(desktopData));
+				metrics.desktop_first_cpu_idle.set({ page }, dataextractor.first_cpu_idle(desktopData));
+				metrics.desktop_interactive.set({ page }, dataextractor.interactive(desktopData));
+				metrics.desktop_speed_index.set({ page }, dataextractor.speed_index(desktopData));
+				metrics.desktop_max_potential_fid.set({ page }, dataextractor.max_potential_fid(desktopData));
+				metrics.desktop_first_meaningful_paint.set({ page }, dataextractor.first_meaningful_paint(desktopData));
+				metrics.desktop_performance_score.set({ page }, dataextractor.performance_score(desktopData));
+				metrics.desktop_accessibility_score.set({ page }, dataextractor.accessibility_score(desktopData));
 			} catch (e) {
 				console.error(`data parsing failed, response dumped, url called: ${page}`);
-				fs.writeFileSync('response.json', data, { mode: 0o755 });
+				fs.writeFileSync('response.json', desktopData, { mode: 0o755 });
+			}
+		}
+		let mobileData = await pagespeed(page, apiKey, 'MOBILE');
+		if (mobileData != null) {
+			try {
+				metrics.mobile_first_contentful_paint.set({ page }, dataextractor.first_contentful_paint(mobileData));
+				metrics.mobile_first_cpu_idle.set({ page }, dataextractor.first_cpu_idle(mobileData));
+				metrics.mobile_interactive.set({ page }, dataextractor.interactive(mobileData));
+				metrics.mobile_speed_index.set({ page }, dataextractor.speed_index(mobileData));
+				metrics.mobile_max_potential_fid.set({ page }, dataextractor.max_potential_fid(mobileData));
+				metrics.mobile_first_meaningful_paint.set({ page }, dataextractor.first_meaningful_paint(mobileData));
+				metrics.mobile_performance_score.set({ page }, dataextractor.performance_score(mobileData));
+				metrics.mobile_accessibility_score.set({ page }, dataextractor.accessibility_score(mobileData));
+			} catch (e) {
+				console.error(`data parsing failed, response dumped, url called: ${page}. Error: ${e.message}`);
+				fs.writeFileSync('response.json', mobileData, { mode: 0o755 });
 			}
 		}
 	}, pollInterval * 60 * 1000);
--- src/pagespeed.js
+++ src/pagespeed.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
-module.exports = async function(page, apiKey) {
-	let url = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${page}&key=${apiKey}&category=PERFORMANCE&category=ACCESSIBILITY`;
+module.exports = async function(page, apiKey, viewport) {
+	let url = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${page}&key=${apiKey}&category=PERFORMANCE&category=ACCESSIBILITY&strategy=${viewport}`;
 	try {
 		const response = await axios.get(url);
 		const data = response.data;
