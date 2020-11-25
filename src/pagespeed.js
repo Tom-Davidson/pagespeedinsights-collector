98ce2caaf719fa704f2865d6913ff0110fdfda57
Anthony Sargeant <anthony.sargeant@sky.uk>
2020-11-25 16:47:12 +0000

    Did some refactoring to remove duplication and decoupled some stuff

--- src/pagespeed.js
+++ src/pagespeed.js
@@ -1,13 +1,22 @@
 const axios = require('axios');
 
-module.exports = async function(page, apiKey, viewport) {
+const viewports = {
+	DESKTOP: 'DESKTOP',
+	MOBILE: 'MOBILE'
+};
+
+const getPagespeedInsights = async (page, apiKey, viewport) => {
 	let url = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${page}&key=${apiKey}&category=PERFORMANCE&category=ACCESSIBILITY&strategy=${viewport}`;
 	try {
 		const response = await axios.get(url);
-		const data = response.data;
-		return data;
+		return response.data;
 	} catch (error) {
 		console.error(`Error fetching ${page} - ${error}`)
 		return null;
 	}
 };
+
+module.exports = {
+	viewports,
+	getPagespeedInsights
+}
